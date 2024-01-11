808d004a3cc6011869183edae1ae219114f0c048
yoganataa <arbiy494@gmail.com>
2024-01-12 00:59:47 +0700

    employee id lang

--- resources/views/employee_dashboard.blade.php
+++ resources/views/employee_dashboard.blade.php
@@ -12,7 +12,7 @@
                 <img alt="" src="{{ asset('assets/img/user.jpg') }}">
             </div>
             <div class="welcome-det">
-                <h3>Welcome, {{ auth()->user()->name }}</h3>
+                <h3>Selamat datang, {{ auth()->user()->name }}</h3>
                 @php
                 $date = date('Y-m-d');
                 $day = date('l', strtotime($date));
@@ -26,7 +26,7 @@
 <div class="row">
     <div class="col-lg-8 col-md-8">
         <section class="dash-section">
-            <h1 class="dash-sec-title">Today</h1>
+            <h1 class="dash-sec-title">Hari ini</h1>
             <div class="dash-sec-content">
                 <div class="dash-info-list">
                     <a href="#" class="dash-card text-danger">
@@ -35,7 +35,7 @@
                                 <i class="fa fa-hourglass-o"></i>
                             </div>
                             <div class="dash-card-content">
-                                <p>Richard Miles is off sick today</p>
+                                <p>Viko sakit hari ini</p>
                             </div>
                             <div class="dash-card-avatars">
                                 <div class="e-avatar"><img src="assets/img/profiles/avatar-09.jpg" alt=""></div>
@@ -51,7 +51,7 @@
                                 <i class="fa fa-suitcase"></i>
                             </div>
                             <div class="dash-card-content">
-                                <p>You are away today</p>
+                                <p>Kamu pergi hari ini</p>
                             </div>
                             <div class="dash-card-avatars">
                                 <div class="e-avatar"><img src="assets/img/profiles/avatar-02.jpg" alt=""></div>
@@ -67,7 +67,7 @@
                                 <i class="fa fa-building-o"></i>
                             </div>
                             <div class="dash-card-content">
-                                <p>You are working from home today</p>
+                                <p>Anda bekerja dari rumah hari ini</p>
                             </div>
                             <div class="dash-card-avatars">
                                 <div class="e-avatar"><img src="assets/img/profiles/avatar-02.jpg" alt=""></div>
@@ -80,7 +80,7 @@
         </section>
 
         <section class="dash-section">
-            <h1 class="dash-sec-title">Tomorrow</h1>
+            <h1 class="dash-sec-title">Besok</h1>
             <div class="dash-sec-content">
                 <div class="dash-info-list">
                     <div class="dash-card">
@@ -89,7 +89,7 @@
                                 <i class="fa fa-suitcase"></i>
                             </div>
                             <div class="dash-card-content">
-                                <p>2 people will be away tomorrow</p>
+                                <p>2 orang akan pergi besok</p>
                             </div>
                             <div class="dash-card-avatars">
                                 <a href="#" class="e-avatar"><img src="assets/img/profiles/avatar-04.jpg" alt=""></a>
@@ -102,7 +102,7 @@
         </section>
 
         <section class="dash-section">
-            <h1 class="dash-sec-title">Next seven days</h1>
+            <h1 class="dash-sec-title">Tujuh hari berikutnya</h1>
             <div class="dash-sec-content">
                 <div class="dash-info-list">
                     <div class="dash-card">
@@ -111,7 +111,7 @@
                                 <i class="fa fa-suitcase"></i>
                             </div>
                             <div class="dash-card-content">
-                                <p>2 people are going to be away</p>
+                                <p>2 orang akan pergi</p>
                             </div>
                             <div class="dash-card-avatars">
                                 <a href="#" class="e-avatar"><img src="assets/img/profiles/avatar-05.jpg" alt=""></a>
@@ -127,7 +127,7 @@
                                 <i class="fa fa-user-plus"></i>
                             </div>
                             <div class="dash-card-content">
-                                <p>Your first day is going to be  on Thursday</p>
+                                <p>Hari pertamamu akan jatuh pada hari Kamis</p>
                             </div>
                             <div class="dash-card-avatars">
                                 <div class="e-avatar"><img src="assets/img/profiles/avatar-02.jpg" alt=""></div>
@@ -142,7 +142,7 @@
                                 <i class="fa fa-calendar"></i>
                             </div>
                             <div class="dash-card-content">
-                                <p>It's Spring Bank Holiday  on Monday</p>
+                                <p>Hari Senin adalah Hari Libur Bank Musim Semi</p>
                             </div>
                         </div>
                     </a>
@@ -154,73 +154,73 @@
     <div class="col-lg-4 col-md-4">
         <div class="dash-sidebar">
             <section>
-                <h5 class="dash-title">Projects</h5>
+                <h5 class="dash-title">Proyek</h5>
                 <div class="card">
                     <div class="card-body">
                         <div class="time-list">
                             <div class="dash-stats-list">
                                 <h4>71</h4>
-                                <p>Total Tasks</p>
+                                <p>Jumlah Tugas</p>
                             </div>
                             <div class="dash-stats-list">
                                 <h4>14</h4>
-                                <p>Pending Tasks</p>
+                                <p>Tugas yang Tertunda</p>
                             </div>
                         </div>
                         <div class="request-btn">
                             <div class="dash-stats-list">
                                 <h4>2</h4>
-                                <p>Total Projects</p>
+                                <p>Jumlah Proyek</p>
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
             <section>
-                <h5 class="dash-title">Your Leave</h5>
+                <h5 class="dash-title">Cuti Anda</h5>
                 <div class="card">
                     <div class="card-body">
                         <div class="time-list">
                             <div class="dash-stats-list">
                                 <h4>4.5</h4>
-                                <p>Leave Taken</p>
+                                <p>Tinggalkan Diambil</p>
                             </div>
                             <div class="dash-stats-list">
                                 <h4>12</h4>
-                                <p>Remaining</p>
+                                <p>Tersisa</p>
                             </div>
                         </div>
                         <div class="request-btn">
-                            <a class="btn btn-primary" href="#">Apply Leave</a>
+                            <a class="btn btn-primary" href="#">Terapkan Cuti</a>
                         </div>
                     </div>
                 </div>
             </section>
             <section>
-                <h5 class="dash-title">Your time off allowance</h5>
+                <h5 class="dash-title">Tunjangan waktu istirahat Anda</h5>
                 <div class="card">
                     <div class="card-body">
                         <div class="time-list">
                             <div class="dash-stats-list">
-                                <h4>5.0 Hours</h4>
-                                <p>Approved</p>
+                                <h4>5.0 Jam</h4>
+                                <p>Disetujui</p>
                             </div>
                             <div class="dash-stats-list">
-                                <h4>15 Hours</h4>
-                                <p>Remaining</p>
+                                <h4>15 Jam</h4>
+                                <p>Tersisa</p>
                             </div>
                         </div>
                         <div class="request-btn">
-                            <a class="btn btn-primary" href="#">Apply Time Off</a>
+                            <a class="btn btn-primary" href="#">Terapkan Waktu Istirahat</a>
                         </div>
                     </div>
                 </div>
             </section>
             <section>
-                <h5 class="dash-title">Upcoming Holiday</h5>
+                <h5 class="dash-title">Liburan Mendatang</h5>
                 <div class="card">
                     <div class="card-body text-center">
-                        <h4 class="holiday-title mb-0">Mon 20 May 2019 - Ramzan</h4>
+                        <h4 class="holiday-title mb-0">Sen 1 Januari 2024 - Turu</h4>
                     </div>
                 </div>
             </section>
